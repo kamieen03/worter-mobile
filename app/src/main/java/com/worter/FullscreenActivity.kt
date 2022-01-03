426c1b27d576eb3ddadee4a357b6e7f9ef842023
kamieen03 <kamieen03@gmail.com>
2022-01-03 18:59:56 +0100

    Add file table

--- app/src/main/java/com/worter/FullscreenActivity.kt
+++ app/src/main/java/com/worter/FullscreenActivity.kt
@@ -5,15 +5,18 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.TextView
+import android.widget.Button
+import android.widget.TableRow
+import androidx.core.content.ContextCompat
 import androidx.core.view.children
+import androidx.core.view.get
 import com.worter.databinding.ActivityFullscreenBinding
-import kotlinx.android.synthetic.main.activity_fullscreen.file_list
+import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 class FullscreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFullscreenBinding
-    private val fileNames = Array(25) {i -> "file$i.json"}
+    private val fileNumbers = Array(50) {i -> "$i"}
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,27 +24,41 @@ class FullscreenActivity : AppCompatActivity() {
         binding = ActivityFullscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        fillFileList()
+        fillFileTable()
     }
 
-    private fun fillFileList() {
-        for (fName in fileNames) {
-            val tv = TextView(this)
-            tv.text = fName
-            tv.gravity = Gravity.CENTER
-            tv.textSize = 30F
-            file_list.addView(tv)
+    private fun fillFileTable() {
+        var col = 0
+        val maxCol = 2
+        var tr = TableRow(this)
+        val firstRow = tr
+        for (fName in fileNumbers) {
+            tr.addView(getFileTableButton(fName))
+            if (col == maxCol) {
+                file_table.addView(tr)
+                tr = TableRow(this)
+            }
+            col = (col+1) % (maxCol + 1)
         }
-
-        file_list.children.forEach {
-            it.setOnClickListener { it1 -> singleFileOnClickListener(it1 as TextView) }
+        if (tr.childCount > 0) {
+            file_table.addView(tr)
         }
+        (firstRow[0] as Button).callOnClick()
     }
 
-    private fun singleFileOnClickListener(tv: TextView) {
-        file_list.children.forEach { it as TextView
-            it.setTextColor(Color.BLACK)
+    private fun getFileTableButton(name: String): Button {
+        val fb = Button(this)
+        fb.text = name
+        fb.setOnClickListener { fileTableButtonOnClickListener(fb)}
+        return fb
+    }
+
+    private fun fileTableButtonOnClickListener(fb: Button) {
+        file_table.children.forEach { it as TableRow
+            it.children.forEach { it1 -> it1 as Button
+                it1.background.setTint(Color.LTGRAY)
+            }
         }
-        tv.setTextColor(Color.YELLOW)
+        fb.background.setTint(ContextCompat.getColor(this, R.color.Bronze))
     }
 }
