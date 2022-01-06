b0edfe8d944737a29d443b5bae722feaab93d644
kamieen03 <kamieen03@gmail.com>
2022-01-06 21:41:33 +0100

    Add basic review activity layout

--- app/src/main/java/com/worter/ReviewActivity.kt
+++ app/src/main/java/com/worter/ReviewActivity.kt
@@ -7,6 +7,8 @@ class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+        supportActionBar?.hide()
+
         val b = this.intent.extras
         val mode = b!!.getString("mode")
     }
