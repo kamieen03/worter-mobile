a21c4d72d1d16a90e3c3b62e99311e3caf9a6458
kamieen03 <kamieen03@gmail.com>
2022-02-21 21:05:50 +0100

    Finish listening

--- app/src/main/java/com/worter/ListeningMenuActivity.kt
+++ app/src/main/java/com/worter/ListeningMenuActivity.kt
@@ -8,13 +8,16 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_listening_menu.*
-import kotlinx.android.synthetic.main.activity_listening_menu.view.*
-import kotlinx.android.synthetic.main.activity_texts_menu.*
+import java.lang.ref.WeakReference
+import java.util.*
+
 
 enum class AudioType {
     SLOW,
@@ -24,23 +27,26 @@ enum class AudioType {
 class ListeningMenuActivity : AppCompatActivity() {
     private val missingFastAudioEpisodes = intArrayOf(23, 34, 47, 71)
     private var missingEpisodesButtons: MutableList<Button> = mutableListOf()
-    private var selectedAudioType = AudioType.SLOW
-    private var selectedIdx: Int = 10
-    var audioPlayer: MediaPlayer? = null
+
+    var selectedAudioType = AudioType.SLOW
+        private set
+    var selectedIdx: Int = 10
+        private set
+    private lateinit var audioController: AudioController
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listening_menu)
+        audioController = AudioController(this)
         fillListeningList()
         listening_flip_audio_type_button.setOnClickListener { flipAudioType() }
         listening_flip_audio_listened_button.setOnClickListener { flipAudioListened() }
-        setAudioControlButtons()
     }
 
     override fun onStop() {
         super.onStop()
-        stopSound()
+        audioController.stopSound()
     }
 
     override fun onPause() {
@@ -82,8 +88,7 @@ class ListeningMenuActivity : AppCompatActivity() {
         fb.background.setTint(ContextCompat.getColor(this, R.color.Bronze))
         selectedIdx = fb.text.toString().toInt()
         setAudioListenedButton(DBManager.isAudioListened(selectedIdx, selectedAudioType)!!)
-        pauseSound()
-        stopSound()
+        audioController.stopSound()
     }
 
     private fun getListeningButtonShape(fb: Button): PaintDrawable {
@@ -104,6 +109,7 @@ class ListeningMenuActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun flipAudioType() {
+        audioController.stopSound()
         if (selectedAudioType == AudioType.SLOW) {
             selectedAudioType = AudioType.FAST
             listening_flip_audio_type_button.text = "FAST"
@@ -153,19 +159,104 @@ class ListeningMenuActivity : AppCompatActivity() {
         DBManager.flipAudioListened(selectedIdx, selectedAudioType)
         setAudioListenedButton(DBManager.isAudioListened(selectedIdx, selectedAudioType)!!)
     }
+}
 
-    private fun setAudioControlButtons(){
-        listening_play_button.setOnClickListener {
+class AudioController(context: ListeningMenuActivity) {
+    private val REWIND_VAL = 10000
+    private val SKIP_VAL = 10000
+
+    private val ctx = WeakReference(context)
+    private var audioPlayer: MediaPlayer? = null
+
+    init {
+        // play / pause
+        ctx.get()!!.listening_play_button.setOnClickListener {
             if (audioPlayer != null && audioPlayer!!.isPlaying) {
                 pauseSound()
             } else {
                 playSound()
             }
         }
+
+        // rewind
+        ctx.get()!!.listening_rewind_button.setOnClickListener {
+            if (audioPlayer != null) {
+                val newTime = maxOf(audioPlayer!!.currentPosition - REWIND_VAL, 0)
+                audioPlayer!!.seekTo(newTime)
+                ctx.get()!!.listening_seek_bar.progress = newTime
+            }
+        }
+
+        // skip
+        ctx.get()!!.listening_skip_button.setOnClickListener {
+            if (audioPlayer != null) {
+                val newTime = minOf(
+                    audioPlayer!!.currentPosition + SKIP_VAL,
+                    audioPlayer!!.duration
+                )
+                audioPlayer!!.seekTo(newTime)
+                ctx.get()!!.listening_seek_bar.progress = newTime
+            }
+        }
+
+        // update seekBar every second
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                ctx.get()!!.listening_seek_bar.progress = if (audioPlayer != null) {
+                    audioPlayer!!.currentPosition
+                } else 0
+            }
+        }, 0, 1000)
+
+        ctx.get()!!.listening_seek_bar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, ms: Int, b: Boolean) {
+                ctx.get()!!.listening_current_time.text = msToTimeString(ms)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 
-    private fun idxToUrl(idx: Int) : String? {
-        return if (selectedAudioType == AudioType.SLOW) {
+    private fun playSound() {
+        if (audioPlayer == null) {
+            audioPlayer = MediaPlayer().apply {
+                setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+                )
+                setDataSource(idxToUrl(ctx.get()!!.selectedIdx))
+                setOnCompletionListener { pauseSound() }
+                prepare()
+                start()
+            }
+            ctx.get()!!.listening_seek_bar.max = audioPlayer!!.duration
+            ctx.get()!!.listening_duration_time.text = msToTimeString(audioPlayer!!.duration)
+        } else audioPlayer!!.start()
+
+        ctx.get()!!.listening_play_button.setImageResource(android.R.drawable.ic_media_pause)
+    }
+
+    private fun pauseSound() {
+        if (audioPlayer != null && audioPlayer!!.isPlaying) audioPlayer!!.pause()
+        ctx.get()!!.listening_play_button.setImageResource(android.R.drawable.ic_media_play)
+    }
+
+    fun stopSound() {
+        pauseSound()
+        if (audioPlayer != null) {
+            audioPlayer!!.reset()
+            ctx.get()!!.listening_duration_time.text = msToTimeString(audioPlayer!!.duration)
+            audioPlayer!!.stop()
+            audioPlayer!!.release()
+            audioPlayer = null
+        }
+    }
+
+    private fun idxToUrl(idx: Int): String? {
+        return if (ctx.get()!!.selectedAudioType == AudioType.SLOW) {
             when (idx) {
                 in 10..172 -> "https://kinderwahnsinn.com/folgen/sg${idx}.mp3"
                 in 173..245 -> "https://cdn.podseed.org/slowgerman/sg${idx}.mp3"
@@ -179,34 +270,12 @@ class ListeningMenuActivity : AppCompatActivity() {
         }
     }
 
-    private fun playSound() {
-        if (audioPlayer == null) {
-            audioPlayer = MediaPlayer().apply {
-                setAudioAttributes(
-                    AudioAttributes.Builder()
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .setUsage(AudioAttributes.USAGE_MEDIA)
-                        .build()
-                )
-                setDataSource(idxToUrl(selectedIdx))
-                prepare()
-                start()
-            }
-        } else audioPlayer!!.start()
-        listening_play_button.setImageResource(android.R.drawable.ic_media_pause)
-    }
-
-    private fun pauseSound() {
-        if (audioPlayer != null && audioPlayer!!.isPlaying) audioPlayer!!.pause()
-        listening_play_button.setImageResource(android.R.drawable.ic_media_play)
-    }
-
-    private fun stopSound() {
-        if (audioPlayer != null) {
-            audioPlayer!!.stop()
-            audioPlayer!!.release()
-            audioPlayer = null
-        }
+    private fun msToTimeString(ms: Int) : String{
+        var s : Int = ms / 1000
+        val min: Int = s / 60
+        s %= 60
+        val sec = if (s < 10) "0$s" else "$s"
+        return "${min}:${sec}"
     }
 
 }
